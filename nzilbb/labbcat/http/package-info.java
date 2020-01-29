291ac4d3c31dafc78d82505656e775b693d64c23
Robert Fromont <robert@fromont.net.nz>
2020-01-29 16:51:47 -0300

    Add generated JavaDoc.

---
+++ nzilbb/labbcat/http/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Helper classes for making HTTP requests.
+ */
+package nzilbb.labbcat.http;
---
+++ nzilbb/labbcat/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * LaBB-CAT client library.
+ * <p>This is client library for communicating with
+ *     <a href="https://labbcat.canterbury.ac.nz/">LaBB-CAT</a>
+ *     web application servers.</p>
+ *   <p>The library provides implementations of 
+ *     <a href="https://nzilbb.github.io/ag/javadoc/nzilbb/ag/IGraphStoreQuery.html">nzilbb.ag.IGraphStoreQuery</a>
+ *     and related interfaces, for standardized API calls.</p>
+ */
+package nzilbb.labbcat;
